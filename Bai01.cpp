6e4468756477a7c04c92fef9e838ed5e3d67e0fa
hokytuyen <54103167+hokytuyen@users.noreply.github.com>
2022-09-16 10:22:37 +0300

    Update Bai01.cpp
    Vì nếu ban đầu S = 0; 
    For loop đầu tiên  S += i => S = 0 + 1;
    cho nên function Calculate(n) = 0 + 1 + 2 + ... + n  Nếu theo sát đề bài 
    Tính S(n) = 1 + 2 + 3 + … + n 
    Thì ko đúng lắm nhỉ? 
    Em sửa lại như vầy ko biết ok ko ạ?

--- Bai01.cpp
+++ Bai01.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int Calculate (int n)
+int Calculate(int n)
 {
 	int S = 0;
-	for (int i = 1 ; i <= n ; i++)
+
+	if (n == 1)
+		S = 1;
+	else if (n <= 0)
+		S = 0;
+	else
 	{
-		S += i;
+		int i = 2;
+		S = 1;
+		while (i <= n)
+		{
+			S += i;
+			i++;
+		}
 	}
 	return S;
 }
